52a5845dd8c1c12a4382bdf0e33247ea2884b3d7
tobias <tobias@refsholt.net>
2023-12-02 16:57:08 +0100

    Refactoring

--- Abaxrekruttering/Program.cs
+++ Abaxrekruttering/Program.cs
@@ -1,17 +1,10 @@
 ﻿using Abaxrekruttering;
 
 var vehicleList = new VehicleList();
-var car1 = new Car("NF123456", 147, VehicleType.LightVehicle, "grønn", 200, VelocityUnit.KilometersPerHour);
-var car2 = new Car("NF654321", 150, VehicleType.LightVehicle, "blå", 195, VelocityUnit.KilometersPerHour);
-var plane = new Plane("LN1234", 1000, VehicleType.JetPlane, 30, 10000, 2000);
-var boat = new Boat("ABC123", 100, VehicleType.Jolle, 30, VelocityUnit.Knot, 500);
 
-vehicleList.AddVehicle(car1);
-vehicleList.AddVehicle(car2);
-vehicleList.AddVehicle(plane);
-vehicleList.AddVehicle(boat);
-
+vehicleList.AddVehicle(new Car("NF123456", 147, VehicleType.LightVehicle, "grønn", 200, VelocityUnit.KilometersPerHour));
+vehicleList.AddVehicle(new Car("NF654321", 150, VehicleType.LightVehicle, "blå", 195, VelocityUnit.KilometersPerHour));
+vehicleList.AddVehicle(new Plane("LN1234", 1000, VehicleType.JetPlane, 30, 10000, 2000));
+vehicleList.AddVehicle(new Boat("ABC123", 100, VehicleType.Jolle, 30, VelocityUnit.Knot, 500));
 vehicleList.ShowAll();
-
-var compareMessage = car1.EqualsTo(car2) ? "Bilene er like" : "Bilene er ikke like";
-Console.WriteLine(compareMessage);
+vehicleList.AreEqual(0, 1);
--- Abaxrekruttering/VehicleList.cs
+++ Abaxrekruttering/VehicleList.cs
@@ -1,3 +1,4 @@
+using System.Reflection.Metadata.Ecma335;
 using System.Text.Json;
 
 namespace Abaxrekruttering;
@@ -11,11 +12,31 @@ class VehicleList
         _vehicles.Add(vehicle);
     }
 
+    public Vehicle GetVehicle(int index)
+    {
+        return _vehicles[index];
+    }
+
     public void ShowAll()
     {
         foreach (var vehicle in _vehicles)
         {
             vehicle.PrintInfo();
         }
+    }
+
+    public void AreEqual(Vehicle vehicle1, Vehicle vehicle2)
+    {
+        if (vehicle1.EqualsTo(vehicle2))
+        {
+            Console.WriteLine("Kjøretøyene er like");
+            return;
+        }
+        Console.WriteLine("Kjøretøyene er ikke like");
+    }
+
+    public void AreEqual(int vehicle1, int vehicle2)
+    {
+        AreEqual(_vehicles[vehicle1], _vehicles[vehicle2]);
     }
 }
